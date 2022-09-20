c76b0f6b6d9908b00f1e9ad422dd203b651784cf
wilson cely <wectcorp@gmail.com>
2022-09-20 10:06:28 +0200

    coding js for the tabs

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,7 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tabs"
 export default class extends Controller {
-  connect() {
-    console.log('hello from stimulus');
+  static targets  = ["tab_left", "tab_right"]
+
+
+  display_info () {
+    if (this.tab_leftTarget.classList.contains('active') && !this.tab_rightTarget.classList.contains('active') )  {
+      console.log("left has the class");
+      this.tab_rightTarget.classList.remove('active')
+      } else if (!this.tab_leftTarget.classList.contains('active') && this.tab_rightTarget.classList.contains('active') )  {
+        console.log("the right has the active class");
+        this.tab_leftTarget.classList.remove('active')
+        this.tab_rightTarget.classList.add('active')
+      } else {
+
+        console.log('other case');
+      }
+
   }
 }
