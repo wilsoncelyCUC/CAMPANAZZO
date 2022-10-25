4da5eff524dd6158486e6007d141162315fff6de
wilson cely <wectcorp@gmail.com>
2022-10-25 12:31:55 +0200

    correction bug validator dateome

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -2,7 +2,7 @@ class AvailabilityValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     bookings = Reservation.where(["profile_id =?", record.profile_id])
-    date_ranges = bookings.map { |b| b.start_date..b.end_date }
+    date_ranges = bookings.map { |b| b.start_date.to_datetime..b.end_date.to_datetime }
 
     date_ranges.each do |range|
       if range.include? value
